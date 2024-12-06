da3314d287a4c9f4b0773bde1bfddbded7707ec6
Daendels <162095067+Daendells@users.noreply.github.com>
2024-12-06 23:49:41 +0700

    Update Main.java
    DVN_23.49

--- src/sudoku/Main.java
+++ src/sudoku/Main.java
@@ -10,14 +10,11 @@
 
 package sudoku;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
-    /** The entry main() entry method */
+  
     public static void main(String[] args) {
-        // [TODO 1] Check "Swing program template" on how to run
-        //  the constructor of "SudokuMain"
-        // .........
+       
         new Sudoku();
     }
 }
