a9c027e8a8563a3abf087ca4aa42b2585b219cdd
Daendels <162095067+Daendells@users.noreply.github.com>
2024-12-20 18:05:44 +0700

    Update Seed.java

--- TicTacToe-AI/TICTACTOE/Seed.java
+++ TicTacToe-AI/TICTACTOE/Seed.java
@@ -1,3 +1,16 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #9
+ * 1 - 5026231131 - Davin Jonathan Tanus
+ * 2 - 5026231115 - Komang Alit Pujangga
+ * 3 - Student ID - Jeremy Danial Haposan Siregar
+ */
+
+
+
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
